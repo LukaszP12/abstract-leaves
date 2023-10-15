fa2ec970f36b23eaeefa6d0d8eaa4cde477a1c9a
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-16 00:10:57 +0200

    DeciduousTree with its leaves specification

---
+++ src/main/java/pl/piwowarski/DeciduousTree.java
@@ -0,0 +1,13 @@
+package pl.piwowarski;
+
+public class DeciduousTree extends Leaf {
+
+    public DeciduousTree(int coneDiameterLength, int twigsNum) {
+        super(coneDiameterLength, twigsNum, "broad and flat leaves");
+    }
+
+    @Override
+    void grow() {
+        System.out.println("Have a re-growth period (often during the summer) to develop the foliage");
+    }
+}
