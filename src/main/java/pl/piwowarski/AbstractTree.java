071325db6e4a91ab7b0f3f8aa53da09b547ef3c2
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-27 00:28:39 +0200

    abstract class with shared fields regarding the cone diamter, number of twigs and leaves

--- src/main/java/pl/piwowarski/AbstractTree.java
+++ src/main/java/pl/piwowarski/AbstractTree.java
@@ -3,15 +3,15 @@ package pl.piwowarski;
 public abstract class AbstractTree {
     private int coneDiameterLength;
     private int twigsNum;
-    private String leaves;
+    private int leaves;
 
-    public AbstractTree(int coneDiameterLength, int twigsNum, String leaves) {
+    public AbstractTree(int coneDiameterLength, int twigsNum, int leaves) {
         this.coneDiameterLength = coneDiameterLength;
         this.twigsNum = twigsNum;
         this.leaves = leaves;
     }
 
-    abstract void grow();
+    abstract void grow(YearPeriod yearPeriod);
 
     public int getConeDiameterLength() {
         return coneDiameterLength;
@@ -26,14 +26,31 @@ public abstract class AbstractTree {
     }
 
     public void setTwigsNum(int twigsNum) {
+        if (twigsNum < 0) {
+            this.twigsNum = 0;
+            return;
+        }
         this.twigsNum = twigsNum;
     }
 
-    public String getLeaves() {
+    public int getLeaves() {
         return leaves;
     }
 
-    public void setLeaves(String leaves) {
+    public void setLeaves(int leaves) {
+        if (leaves < 0) {
+            this.leaves = 0;
+            return;
+        }
         this.leaves = leaves;
+    }
+
+    @Override
+    public String toString() {
+        return "after some time growing, the tree looks like: " +
+                "coneDiameterLength=" + coneDiameterLength +
+                ", twigsNum=" + twigsNum +
+                ", leaves='" + leaves + '\'' +
+                '}';
     }
 }
