04be4fe67be84ba002d60907812a41012046f5ca
Lukasz Piwowarski <lukaszp4@onet.eu>
2023-10-27 00:30:49 +0200

    concrete class of a ConiferTree, which preserves leaves through winter / konkretna klas drzewa iglastego, które zachowuje igły przez zimę

--- src/main/java/pl/piwowarski/ConiferTree.java
+++ src/main/java/pl/piwowarski/ConiferTree.java
@@ -1,13 +1,41 @@
 package pl.piwowarski;
 
+/**
+ * ConiferTree have more sort of needles as leaves and keep them thorough the whole winter
+ * Drzewa iglaste posiadają raczej igły, niż tradycyjne liście i zachowują je przez całą zimę
+ **/
+
 public class ConiferTree extends AbstractTree {
 
-    public ConiferTree(int coneDiameterLength, int twigsNum) {
-        super(coneDiameterLength, twigsNum, "small needle like leaves");
+    public ConiferTree(int coneDiameterLength, int twigsNum, int leaves) {
+        super(coneDiameterLength, twigsNum, leaves);
     }
 
     @Override
-    void grow() {
-        System.out.println("ConiferTrees are evergreen, so they don't re-grow");
+    void grow(YearPeriod yearPeriod) {
+        switch (yearPeriod) {
+            case AUTUMN:
+                super.setLeaves(getLeaves() + 50);
+                super.setTwigsNum(getTwigsNum() + 1);
+                break;
+            case WINTER:
+                super.setTwigsNum(getTwigsNum() - 2);
+                break;
+            case SPRING:
+                super.setLeaves(getLeaves() + 150);
+                super.setTwigsNum(getTwigsNum() + 2);
+                break;
+            case SUMMER:
+                super.setLeaves(getLeaves() + 100);
+                super.setTwigsNum(getTwigsNum() + 1);
+                break;
+        }
+        super.setConeDiameterLength(getConeDiameterLength() + 4);
+        System.out.println("ConiferTrees are evergreen, so they don't re-grow" + " our tree is growing through " + yearPeriod);
+    }
+
+    @Override
+    public String toString() {
+        return "ConiferTree{} " + super.toString();
     }
 }
